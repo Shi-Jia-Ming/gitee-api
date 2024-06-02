806128aef2fd84f5f22ccf971c5df1ed1f28f968
Shi-Jia-Ming <sjiaming38@gmail.com>
2024-06-02 23:41:45 +0800

    test($service $authorization): finish user authorization function test
    add to test instance:
    - `generateAccessTokenByPassword async`
    - `generateAccessTokenByPassword callback`

--- src/__tests__/service/authorization.service.test.ts
+++ src/__tests__/service/authorization.service.test.ts
@@ -1,27 +1,63 @@
 import { AuthorizationService } from "../../service/authorization.service";
 import TokenInfo from "../../entity/token.info";
+import * as dotenv from "dotenv";
 
-test("generateAccessTokenByCode async", async () => {
-  const tokenInfo = await AuthorizationService.generateAccessTokenByCode(
-    "e5110e3c9c9f9ddae749f3c840a032f15f8373b9421032b445429b0fec84b2e2",
-    "9d5424c75ba160e8c4d3f7293a750fddf6cf131b5ebfd55200c106f9cddb286c",
-    "https://gitee.com/stunning-head-cub",
-    "1342ac77912f3ef2e85e2294cbdb3e9d82258112349b11f674934f4ebe61f047",
+dotenv.config();
+
+/**
+ * the code should generate in per test, so it will be closed after the test run successfully
+ */
+// test("generateAccessTokenByCode async", async () => {
+//   const tokenInfo = await AuthorizationService.generateAccessTokenByCode(
+//     "e5110e3c9c9f9ddae749f3c840a032f15f8373b9421032b445429b0fec84b2e2",
+//     process.env.CLIENT_ID,
+//     process.env.REDIRECT_URI,
+//     process.env.CLIENT_SECRET,
+//   );
+//   console.log(tokenInfo.accessToken);
+//   console.log(tokenInfo.refreshToken);
+// });
+
+/**
+ * the code should generate in per test, so it will be closed after the test run successfully
+ */
+// test("generateAccessTokenByCode callback", () => {
+//   AuthorizationService.generateAccessTokenByCode(
+//     "1639715cf376b9c2c4b36dcd517e27a4c4d57bcf4d10686bf6c6a36452b60a3b",
+//     process.env.CLIENT_ID,
+//     process.env.REDIRECT_URI,
+//     process.env.CLIENT_SECRET,
+//     (err: any, data: TokenInfo | undefined) => {
+//       if (!err) {
+//         console.log(data?.accessToken);
+//         console.log(data?.refreshToken);
+//       }
+//     });
+// });
+
+test("generateAccessTokenByPassword async", async () => {
+  const tokenInfo: TokenInfo = await AuthorizationService.generateAccessTokenByPassword(
+    process.env.EMAIL as string,
+    process.env.PASSWORD as string,
+    process.env.CLIENT_ID as string,
+    process.env.CLIENT_SECRET as string
   );
-  console.log(tokenInfo.accessToken);
-  console.log(tokenInfo.refreshToken);
-});
 
-test("generateAccessTokenByCode callback", () => {
-  AuthorizationService.generateAccessTokenByCode(
-    "1639715cf376b9c2c4b36dcd517e27a4c4d57bcf4d10686bf6c6a36452b60a3b",
-    "9d5424c75ba160e8c4d3f7293a750fddf6cf131b5ebfd55200c106f9cddb286c",
-    "https://gitee.com/stunning-head-cub",
-    "1342ac77912f3ef2e85e2294cbdb3e9d82258112349b11f674934f4ebe61f047",
-    (err: any, data: TokenInfo | undefined) => {
+  console.log(tokenInfo.accessToken);
+  console.log(tokenInfo.createdAt);
+})
+
+test("generateAccessTokenByPassword callback", () => {
+  AuthorizationService.generateAccessTokenByPassword(
+    process.env.EMAIL as string,
+    process.env.PASSWORD as string,
+    process.env.CLIENT_ID as string,
+    process.env.CLIENT_SECRET as string,
+    (err: unknown, data: undefined | TokenInfo) => {
       if (!err) {
         console.log(data?.accessToken);
-        console.log(data?.refreshToken);
+        console.log(data?.createdAt);
       }
-    });
-});
+    }
+  )
+})
