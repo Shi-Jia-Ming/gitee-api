cd75f5445b3d055567e57cb160f59d5f70c3fc09
Shi-Jia-Ming <sjiaming38@gmail.com>
2024-06-02 22:56:22 +0800

    refactor($entity): move entity to new file
    - move namespace entity to namespace.info.ts
    - move repository entity to repository.info.ts

--- src/entity/activity.info.ts
+++ src/entity/activity.info.ts
@@ -1,5 +1,6 @@
 import OrganizationInfo from "./organization.info";
 import UserInfo from "./user.info";
+import RepositoryInfo from "./repository.info";
 
 /**
  * activity info entity
@@ -22,7 +23,7 @@ export default class ActivityInfo {
   /**
    * repository of activity
    */
-  public repo?: Repo;
+  public repo?: RepositoryInfo;
   /**
    * organization of activity
    */
@@ -39,76 +40,6 @@ export default class ActivityInfo {
    * payload of activity
    */
   public payload?: any;
-};
-
-/**
- * repository entity of activity
- */
-export class Repo {
-  /**
-   * repository id
-   */
-  public id?: number;
-  /**
-   * full name of repository
-   */
-  public fullName?: string;
-  /**
-   * name of repository
-   */
-  public humanName?: string;
-  /**
-   * url of repository
-   */
-  public url?: string;
-  /**
-   * namespace belongs to
-   */
-  public namespace?: Namespace;
-};
-
-/**
- * namespace entity
- */
-export class Namespace {
-  /**
-   * namespace id
-   */
-  public id?: number;
-  /**
-   * name of namespace
-   */
-  public name?: string;
-  /**
-   * namespace type
-   */
-  public type?: NamespaceType;
-  /**
-   * path of namespace
-   */
-  public path?: string;
-  /**
-   * url of namespace space
-   */
-  public htmlUrl?: string;
-};
-
-/**
- * namespace type enum
- */
-export enum NamespaceType {
-  /**
-   * namespace type: enterprise
-   */
-  ENTERPRISE,
-  /**
-   * namespace type: user
-   */
-  NULL,
-  /**
-   * namespace type: organization
-   */
-  GROUP
 }
 
 /**
@@ -131,4 +62,4 @@ export enum ActivityType {
    * event type: create
    */
   CREATE_EVENT
-};
+}
