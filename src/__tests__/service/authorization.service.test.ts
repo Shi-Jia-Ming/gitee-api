cb552fbecb9eb4d75e056cec951a8928fc52337a
Shi-Jia-Ming <sjiaming38@gmail.com>
2024-06-03 15:11:49 +0800

    test($service $authorization): finish user refresh access token test
    add to test instance:
    - `refreshAccessToken async`
    - `refreshAccessToken callback`

--- src/__tests__/service/authorization.service.test.ts
+++ src/__tests__/service/authorization.service.test.ts
@@ -4,36 +4,30 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-/**
- * the code should generate in per test, so it will be closed after the test run successfully
- */
-// test("generateAccessTokenByCode async", async () => {
-//   const tokenInfo = await AuthorizationService.generateAccessTokenByCode(
-//     "e5110e3c9c9f9ddae749f3c840a032f15f8373b9421032b445429b0fec84b2e2",
-//     process.env.CLIENT_ID,
-//     process.env.REDIRECT_URI,
-//     process.env.CLIENT_SECRET,
-//   );
-//   console.log(tokenInfo.accessToken);
-//   console.log(tokenInfo.refreshToken);
-// });
+test("generateAccessTokenByCode async", async () => {
+  const tokenInfo = await AuthorizationService.generateAccessTokenByCode(
+    "e5110e3c9c9f9ddae749f3c840a032f15f8373b9421032b445429b0fec84b2e2",
+    process.env.CLIENT_ID as string,
+    process.env.REDIRECT_URI as string,
+    process.env.CLIENT_SECRET as string,
+  );
+  console.log(tokenInfo.accessToken);
+  console.log(tokenInfo.createdAt);
+});
 
-/**
- * the code should generate in per test, so it will be closed after the test run successfully
- */
-// test("generateAccessTokenByCode callback", () => {
-//   AuthorizationService.generateAccessTokenByCode(
-//     "1639715cf376b9c2c4b36dcd517e27a4c4d57bcf4d10686bf6c6a36452b60a3b",
-//     process.env.CLIENT_ID,
-//     process.env.REDIRECT_URI,
-//     process.env.CLIENT_SECRET,
-//     (err: any, data: TokenInfo | undefined) => {
-//       if (!err) {
-//         console.log(data?.accessToken);
-//         console.log(data?.refreshToken);
-//       }
-//     });
-// });
+test("generateAccessTokenByCode callback", () => {
+  AuthorizationService.generateAccessTokenByCode(
+    "1639715cf376b9c2c4b36dcd517e27a4c4d57bcf4d10686bf6c6a36452b60a3b",
+    process.env.CLIENT_ID as string,
+    process.env.REDIRECT_URI as string,
+    process.env.CLIENT_SECRET as string,
+    (err: any, data: TokenInfo | undefined) => {
+      if (!err) {
+        console.log(data?.accessToken);
+        console.log(data?.refreshToken);
+      }
+    });
+});
 
 test("generateAccessTokenByPassword async", async () => {
   const tokenInfo: TokenInfo = await AuthorizationService.generateAccessTokenByPassword(
@@ -45,7 +39,7 @@ test("generateAccessTokenByPassword async", async () => {
 
   console.log(tokenInfo.accessToken);
   console.log(tokenInfo.createdAt);
-})
+});
 
 test("generateAccessTokenByPassword callback", () => {
   AuthorizationService.generateAccessTokenByPassword(
@@ -60,4 +54,25 @@ test("generateAccessTokenByPassword callback", () => {
       }
     }
   )
-})
+});
+
+test.only("refreshAccessToken async", async () => {
+  const tokenInfo: TokenInfo = await AuthorizationService.refreshAccessToken(
+    process.env.REFRESH_TOKEN as string
+  );
+
+  console.log(tokenInfo.accessToken);
+  console.log(tokenInfo.createdAt);
+});
+
+test("refreshAccessToken callback", () => {
+  AuthorizationService.refreshAccessToken(
+    process.env.REFRESH_TOKEN as string,
+    (err: unknown, data: undefined | TokenInfo) => {
+      if (!err) {
+        console.log(data?.accessToken);
+        console.log(data?.createdAt);
+      }
+    }
+  )
+});
